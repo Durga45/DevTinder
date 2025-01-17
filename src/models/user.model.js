e3392ec30c48536167c9dfd64f936850853e2df5
Durga45 <dspandey051@gmail.com>
2025-01-17 15:37:16 +0530

    middleware

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,66 +1,70 @@
 import mongoose from "mongoose";
 const { Schema } = mongoose;
 
-// Define allowed gender values for better readability
 const GENDER_VALUES = ["male", "female", "other"];
 
-const userSchema = new Schema({
-  firstName: {
-    type: String,
-    required: true,
-    minLength: 3,
-    trim: true  // Automatically removes leading/trailing spaces
+const userSchema = new Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      minLength: 3,
+      trim: true,
+    },
+    lastName: {
+      type: String,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      lowercase: true,
+      trim: true,
+      unique: true,  // Ensures no duplicate emails
+      match: [
+        /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/,
+        "Please fill a valid email address",
+      ],
+    },
+    password: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    age: {
+      type: Number,
+      required: false,
+      min: 18,
+    },
+    gender: {
+      type: String,
+      required: false,
+      validate(value) {
+        if (!GENDER_VALUES.includes(value)) {
+          throw new Error("Not a valid gender");
+        }
+      },
+    },
+    photoUrl: {
+      type: String,
+      default:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTpCKq1XnPYYDaUIlwlsvmLPZ-9-rdK28RToA&s",
+    },
+    about: {
+      type: String,
+      default: "This is a default about user",
+      trim: true,
+    },
+    skills: {
+      type: [String],
+      default: [],
+    },
   },
-  lastName: {
-    type: String,
-    trim: true  // Automatically removes leading/trailing spaces
-  },
-  email: {
-    type: String,
-    required: true,
-    lowercase: true,
-    trim: true,
-    unique: true,  // Ensures no duplicate emails
-    match: [/^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/, 'Please fill a valid email address']  // Email format validation
-  },
-  password: {
-    type: String,
-    required: true,
-    trim: true
-  },
-  age: {
-    type: Number,
-    required: true,
-    min: 18
-  },
-  gender: {
-    type: String,
-    required: true,
-    validate(value) {
-      if (!GENDER_VALUES.includes(value)) {
-        throw new Error("Not a valid gender");
-      }
-    }
-  },
-  photoUrl: {
-    type: String,
-    default: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTpCKq1XnPYYDaUIlwlsvmLPZ-9-rdK28RToA&s"
-  },
-  about: {
-    type: String,
-    default: "This is a default about user",
-    trim: true  // Automatically removes leading/trailing spaces
-  },
-  skills: {
-    type: [String],
-    default: []  // Ensures that it’s an empty array if no skills are provided
+  {
+    timestamps: true,
   }
-},
-{
-  timestamps:true
-}
 );
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 
 export default User;
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -2,6 +2,8 @@ import express from "express";
 import User from "../models/user.model.js";
 import bcrypt from "bcrypt";
 import { z } from "zod";
+import jwt from "jsonwebtoken"
+import middlewareAuth from "../middleware/middleware.js";
 
 const userRouter = express.Router();
 
@@ -15,12 +17,15 @@ const userSchema = z.object({
     .regex(/[A-Z]/)
     .regex(/[0-9]/)
     .regex(/[^A-Za-z0-9]/),
-  age: z.number().int().positive().min(18),
-  gender: z.enum(["male", "female", "other"]),
 });
 
+const loginSchema=z.object({
+  email: z.string().email(),
+  password: z.string().min(8)
+})
+
 userRouter.post("/user/signup", async (req, res) => {
-  const { firstName, lastName, email, password, age, gender, photoUrl, about, skills } = req.body;
+  const { firstName, lastName, email, password } = req.body;
 
   try {
     const parsedData = userSchema.safeParse(req.body);
@@ -41,11 +46,6 @@ userRouter.post("/user/signup", async (req, res) => {
       lastName,
       email,
       password: hashedPassword,
-      age,
-      gender,
-      photoUrl,
-      about,
-      skills
     });
 
     await newUser.save();
@@ -56,5 +56,104 @@ userRouter.post("/user/signup", async (req, res) => {
     res.status(500).json({ message: "Internal server error" });
   }
 });
+
+//login
+userRouter.post('/user/login', async (req, res) => {
+  const { email, password } = req.body;
+  try {
+      const parsedData = loginSchema.safeParse(req.body);
+      if (!parsedData.success) {
+          return res.status(400).json({ message: parsedData.error.errors });
+      }
+
+      const userExist = await User.findOne({ email });
+
+      if (!userExist) {
+          return res.status(400).json({ message: "Invalid credentials" });
+      }
+
+      const isValidPassword = await bcrypt.compare(password, userExist.password);
+
+      if (isValidPassword) {
+        const token=jwt.sign({userid:userExist._id},process.env.JWTKEY,{expiresIn:"1h"})
+        return  res.status(200).send({ message: "Login successful",token:token });
+      } else {
+          return res.status(400).json({ message: "Invalid credentials" });
+      }
+  } catch (err) {
+      console.error(err);
+      res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+//profile
+userRouter.get("/user/profile",middlewareAuth,async(req,res)=>{
+    const userid=req.userid
+    try {
+      const user = await User.findById(userid).select('-password');; 
+      
+      if (!user) {
+          return res.status(404).json({ message: 'User not found' });
+      }
+      
+      res.status(200).json({ message: 'User profile', user });
+  } catch (err) {
+      console.error(err);
+      res.status(500).json({ message: 'Internal server error' });
+  }
+})
+
+userRouter.patch("/user/update/:id", middlewareAuth, async (req, res) => {
+  const userid = req.userid;
+  const userId = req.params.id;
+  const fieldsToUpdate = req.body;  
+
+  if (userid !== userId) {
+    return res.status(403).json({ message: "You can only update your own profile" });
+  }
+
+  const validFields = ["skills", "password", "about", "photoUrl"];
+
+  const filteredFields = Object.keys(fieldsToUpdate)
+    .filter(field => validFields.includes(field))
+    .reduce((obj, key) => {
+      obj[key] = fieldsToUpdate[key];
+      return obj;
+    }, {});
+
+  if (Object.keys(filteredFields).length === 0) {
+    return res.status(400).json({ message: "No valid fields to update" });
+  }
+
+  if (filteredFields.password) {
+    try {
+      const hashedPassword = await bcrypt.hash(filteredFields.password, 10);
+      filteredFields.password = hashedPassword;
+    } catch (error) {
+      return res.status(500).json({ message: "Error hashing password" });
+    }
+  }
+
+  try {
+    const user = await User.findById(userId);
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    Object.keys(filteredFields).forEach((key) => {
+      user[key] = filteredFields[key];  
+    });
+
+    await user.save();
+
+    res.status(200).json({ message: "User updated successfully", user });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+
 
 export default userRouter;
